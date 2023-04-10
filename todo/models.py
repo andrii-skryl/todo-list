c2d34609e5f59fd8428079da21cd3b27c702b406
Andrii Skryl <a.skryl92@gmail.com>
2023-04-10 22:33:24 +0300

    Created models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField()
+    deadline = models.DateTimeField(null=True, blank=True)
+    status = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks")
