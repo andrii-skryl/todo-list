e3d6983e420c02385dbf9fe2244a5d3924785439
Andrii Skryl <a.skryl92@gmail.com>
2023-04-10 22:56:38 +0300

    Created views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
+from django.views import generic
 from django.shortcuts import render
 
-# Create your views here.
+from todo.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TagListView(generic.ListView):
+    model = Tag
