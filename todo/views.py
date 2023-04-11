230a1a6c3948c03ece0b199ccef3c6ea373b6c67
Andrii Skryl <a.skryl92@gmail.com>
2023-04-11 19:02:12 +0300

    Created views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render
 
@@ -8,5 +9,39 @@ class TaskListView(generic.ListView):
     model = Task
 
 
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:tag-list")
